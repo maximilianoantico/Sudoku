ffacd977ddd83b5ccb534031a6db2bda05ef63a2
maximilianoantico <84809110+maximilianoantico@users.noreply.github.com>
2021-05-27 21:31:15 -0300

    Update jugar_sudoku.py

--- jugar_sudoku.py
+++ jugar_sudoku.py
@@ -2,17 +2,20 @@ from mapas import MAPAS
 import sudoku
 import random
 import copy
-
+VACIO = 0
 
 def imprimir_sudoku(Sudoku):
-    VACIO = 0
     orden_fila = 1
-    valor_columnas = ("   1"+"  2"+"  3"+"  4"+"  5"+"  6"+"  7"+"  8"+"  9")
+    valor_columnas = ("  "+"   1"+"  2"+"  3"+"  4"+"  5"+"  6"+"  7"+"  8"+"  9")
+    borde_superior = ("  "+"   _"+"  _"+"  _"+"  _"+"  _"+"  _"+"  _"+"  _"+"  _")
+    borde_inferior = ("  "+"   -"+"  -"+"  -"+"  -"+"  -"+"  -"+"  -"+"  -"+"  -")
     print("""
 ╔╦╗╦ ╦╦╔═╗  ╦╔═╗  ╔═╗╦ ╦╔╦╗╔═╗╦╔═╦ ╦  
  ║ ╠═╣║╚═╗  ║╚═╗  ╚═╗║ ║ ║║║ ║╠╩╗║ ║  
- ╩ ╩ ╩╩╚═╝  ╩╚═╝  ╚═╝╚═╝═╩╝╚═╝╩ ╩╚═╝ """)
+ ╩ ╩ ╩╩╚═╝  ╩╚═╝  ╚═╝╚═╝═╩╝╚═╝╩ ╩╚═╝ 
+ """)
     print (valor_columnas)
+    print (borde_superior)
     for fila in Sudoku:
         filas = ""
         
@@ -21,63 +24,112 @@ def imprimir_sudoku(Sudoku):
                 filas += "|.|"
             if valor != VACIO:
                 filas += ("|" + str(valor) + "|")
-        print (orden_fila, filas, orden_fila)
+        print (orden_fila, "-", filas, "-", orden_fila)
         orden_fila +=1
+    print (borde_inferior)
     print (valor_columnas)
     
 def main():
-    VACIO = 0
     Sudoku = sudoku.crear_juego(juego = random.choice(MAPAS))
     imprimir_sudoku(Sudoku)
     while True:
-        accion = int(input("1-Agregar Valor\n2-Borrar Valor\n3-Salir\n\nIngrese acción: "))
-        
+        accion = input("\n1-Agregar Valor\n2-Borrar Valor\n3-Salir\n\nIngrese acción: ")
+        if (not accion in "123") or accion == "" :
+            print("""
+                  Eliga una de las 3 opciones
+                  """)
+            imprimir_sudoku(Sudoku)
+        else:
+            accion = int(accion)
         if accion == 1:
-             fila = (int(input ("Ingrese fila (Arriba a abajo) del 1 al 9 donde quiere poner su valor: ")))-1
-             columna = (int(input("Ingrese columna (izquierda a derecha) del 1 al 9 donde quiere poner su valor: ")))-1
-             valor = int(input ("Ingrese un valor del 1 al 9: "))
-             if True == (0 <=columna<= 8 and 1 <= valor <= 9 and  0 <=fila<= 8):
-                 if (True == sudoku.es_movimiento_valido(Sudoku, fila, columna, valor)) and (True == (sudoku.obtener_valor(Sudoku, fila, columna) == VACIO)):
+            fila = input("Ingrese fila (Arriba a abajo) del 1 al 9 donde quiere poner su valor: ")
+            if (not fila in "123456789") or fila == "":
+                print("""
+                      El valor ingresado no es valido
+                      """)
+                imprimir_sudoku(Sudoku)
+                continue
+            else:
+                fila = (int(fila)) - 1 
+            columna = input ("Ingrese columna (izquierda a derecha) del 1 al 9 donde quiere poner su valor: ")
+            if (not columna in "123456789") or columna == "":
+                print("""
+                      El valor ingresado no es valido
+                      """)
+                imprimir_sudoku(Sudoku)
+                continue
+            else:
+                columna = (int(columna)) - 1
+            valor = input ("Ingrese un valor del 1 al 9: ")
+            if (not valor in "123456789") or valor == "":
+                print("""
+                      El valor ingresado no es valido
+                      """)
+                imprimir_sudoku(Sudoku)
+                continue
+            else: 
+                valor = int(valor)
+            if (0 <=columna<= 8) and (1 <= valor <= 9) and  (0 <=fila<= 8):
+                 if (sudoku.es_movimiento_valido(Sudoku, fila, columna, valor) and (sudoku.obtener_valor(Sudoku, fila, columna) == VACIO)):
                      imprimir_sudoku(sudoku.insertar_valor(Sudoku, fila, columna, valor))
                      Sudoku = copy.deepcopy(sudoku.insertar_valor(Sudoku, fila, columna, valor))
                  else:
                      print("""
                            Movimiento no es valido hay algun valor repetido en alguna parte
                            
-                           Tambien pudo ser que se ingreso cualquier cosa jeje""")
+                           """)
                      imprimir_sudoku(Sudoku)  
-             else:
+            else:
                  print("""
                            Movimiento no es valido hay algun valor repetido en alguna parte
                            
-                           Tambien pudo ser que se ingreso cualquier cosa jeje""")
+                           """)
                  imprimir_sudoku(Sudoku)  
-        else:
-            pass
+
         
-        if accion == 2:
-             fila = (int(input ("Ingrese fila (Arriba a abajo) del 1 al 9 donde borrar su valor: ")))-1
-             columna = (int(input("Ingrese columna (izquierda a derecha) del 1 al 9 donde quiere borrar su valor: ")))-1
-             valor = int(input ("Ingrese el valor del 1 al 9 que quiere borrar: "))
-             if True == (0 <=columna<= 8 and 1 <= valor <= 9 and  0 <=fila<= 8):
+        elif accion == 2:
+            fila = input ("Ingrese fila (Arriba a abajo) del 1 al 9 donde quiere poner su valor: ")
+            if (not fila in "123456789") or fila == "":
+                print("""
+                      El valor ingresado no es valido
+                      """)
+                imprimir_sudoku(Sudoku)
+                continue
+            else:
+                fila = (int(fila)) - 1 
+            columna = input ("Ingrese columna (izquierda a derecha) del 1 al 9 donde quiere poner su valor: ")
+            if (not columna in "123456789") or columna == "":
+                print("""
+                      El valor ingresado no es valido
+                      """)
+                imprimir_sudoku(Sudoku)
+                continue
+            else:
+                columna = (int(columna)) - 1
+            valor = input ("Ingrese un valor del 1 al 9: ")
+            if (not valor in "123456789") or valor == "":
+                print("""
+                      El valor ingresado no es valido
+                      """)
+                imprimir_sudoku(Sudoku)
+                continue
+            else: 
+                valor = int(valor)
+            if 0 <=columna<= 8 and 1 <= valor <= 9 and  0 <=fila<= 8:
                  imprimir_sudoku(sudoku.borrar_valor(Sudoku, fila, columna))
                  Sudoku = copy.deepcopy(sudoku.borrar_valor(Sudoku, fila, columna))
-             else:
+            else:
                  print("""
                        Movimiento no es valido hay algun valor repetido en alguna parte
-                       
-                       Tambien pudo ser que se ingreso cualquier cosa jeje""")
+                       """)
                  imprimir_sudoku(Sudoku)
-                 
-        else:
-            pass
         
-        if accion == 3:
+        elif accion == 3:
             print("Te has retirado! Sera hasta la proxima")
             return False
         
         if True == sudoku.esta_terminado(Sudoku):
-             print("""
+            print("""
       ______     _  _        _  _                _                             _____                            _         _  _  _ 
      |  ____|   | |(_)      (_)| |              (_)                           / ____|                          | |       | || || |
      | |__  ___ | | _   ___  _ | |_  __ _   ___  _   ___   _ __    ___  ___  | |  __   __ _  _ __    __ _  ___ | |_  ___ | || || |
@@ -86,15 +138,17 @@ def main():
      |_|   \___||_||_| \___||_| \__|\__,_| \___||_| \___/ |_| |_| \___||___/  \_____| \__,_||_| |_| \__,_||___/ \__|\___|(_)(_)(_)
                                                                                                                                   
                                                                                                                                   """)
-             repetir = int (input("Queres seguir jugando?\n1-Pasame otro que lo completo en un PIM PAM PUM \n2-No, necesito un descansito\nOpcion Seleccionada: "))
-             if repetir == 1:
-                 return main()
-             else:
+            repetir = input("Queres seguir jugando?\n1-Pasame otro que lo completo en un PIM PAM PUM \n2-No, necesito un descansito\nOpcion Seleccionada: ")
+             
+            if (not repetir in "12") and repetir == "":
+                print("""El valor ingresado no es valido """)
+                continue
+            elif int(repetir) == 1:
+                Sudoku = sudoku.crear_juego(juego = random.choice(MAPAS))
+                continue
+            elif int(repetir) == 2:
                  break
         
 
 
 main()
-    
-    
-   
